6dd3f755b68974185be372d9bd189ca682cd378e
ezra <ezra.mizrahi@hey.com>
2024-01-26 17:43:03 +1100

    testing css

--- app/main.py
+++ app/main.py
@@ -1,9 +1,12 @@
 from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from app.routers.scraping_router import router as scraping_router
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 app.include_router(scraping_router)
 
