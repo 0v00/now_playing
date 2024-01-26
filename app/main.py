7daf52d3b4e3dd926b49c6e4f4dcb98bec5370e5
ezra <ezra.mizrahi@hey.com>
2024-01-26 14:56:56 +1100

    add jinja, add htmx

--- app/main.py
+++ app/main.py
@@ -1,6 +1,12 @@
-from fastapi import FastAPI
-from app.routers import scraping_router
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+from app.routers.scraping_router import router as scraping_router
 
 app = FastAPI()
+templates = Jinja2Templates(directory="templates")
 
-app.include_router(scraping_router.router)
+app.include_router(scraping_router)
+
+@app.get("/")
+async def read_root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
