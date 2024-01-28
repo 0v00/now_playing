2d4a8deff0825f5de32b45c80371ef740cb050b2
ezra <ezra.mizrahi@hey.com>
2024-01-28 18:00:19 +1100

    change names

--- app/main.py
+++ app/main.py
@@ -1,14 +1,14 @@
 from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-from app.routers.scraping_router import router as scraping_router
+from app.routers.movies_router import router as movies_router
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-app.include_router(scraping_router)
+app.include_router(movies_router)
 
 @app.get("/")
 async def read_root(request: Request):
---
+++ app/routers/movies_router.py
@@ -0,0 +1,14 @@
+from fastapi import APIRouter, HTTPException, Request
+from fastapi.templating import Jinja2Templates
+from app.database.db_utils import get_movies_with_showtimes
+
+router = APIRouter()
+templates = Jinja2Templates(directory="templates")
+
+@router.get("/get-movies")
+async def get_movies(request: Request):
+    try:
+        movies_data = get_movies_with_showtimes()
+        return templates.TemplateResponse("movies_list.html", {"request": request, "movies": movies_data})
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
